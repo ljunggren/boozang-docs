9b00be08d5f67d28a0bb1b86f6a88e2ca30d5487
ljunggren <mljunggren@gmail.com>
2019-09-25 16:30:05 -0400

    Some useful scripts

--- process.sh
+++ process.sh
@@ -3,6 +3,9 @@ couscous generate
 rm -rf .couscous/generated/public/
 cp -r .couscous/generated/* public/
 git add public/*
+git add images/*
 git add README.md
 git commit -m "Updated docs"
 git push
+
+ssh centos@bld1be.boozang.com  'scripts/update-docs.sh'
